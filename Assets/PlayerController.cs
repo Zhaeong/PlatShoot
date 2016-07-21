50caeea4ab468d8b1cc9aaeb2ce9606af7202821
Zhaeong <owen_zzz@hotmail.com>
2016-07-21 18:19:28 -0400

    first commit dogs

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody2D curRigidbody;
+
+    // Use this for initialization
+    void Start () {
+        curRigidbody = gameObject.GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+
+
+    }
+
+    void FixedUpdate()
+    {
+        float input_x = Input.GetAxisRaw("Horizontal");
+        float input_y = Input.GetAxisRaw("Vertical");
+        curRigidbody.velocity = new Vector2(input_x * speed, input_y * speed);
+
+    }
+
+    void flip()
+    {
+        foreach (Transform child in transform)
+        {
+            Vector3 theScale = child.transform.localScale;
+            theScale.x *= -1;
+            child.transform.localScale = theScale;
+        }
+    }
+}
---
+++ Assets/ProjectileController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileController : MonoBehaviour {
+
+    public float ProjectileSpeed;
+    public GameObject Projectile;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(KeyCode.E))
+        {
+            Instantiate(Projectile, gameObject.transform.position, gameObject.transform.rotation);
+            Projectile.GetComponent<Rigidbody2D>().velocity = new Vector2(ProjectileSpeed, 0);
+        }
+
+	
+	}
+}
