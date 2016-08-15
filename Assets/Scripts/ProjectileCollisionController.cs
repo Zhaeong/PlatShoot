62aef4901528ee8c3b8d0222ebe77dcbfad0b5fd
Zhaeong <owen_zzz@hotmail.com>
2016-08-15 01:23:19 -0400

    destroy projectiles

---
+++ Assets/Scripts/ProjectileCollisionController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectileCollisionController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Destroy(gameObject, 3);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        Destroy(gameObject);
+
+    }
+
+}
