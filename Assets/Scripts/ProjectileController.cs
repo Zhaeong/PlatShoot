48e29c2c7479e6b3a9b3fd464048576ddbc9c1b6
Zhaeong <owen_zzz@hotmail.com>
2016-08-14 21:10:41 -0400

    rotation

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -5,6 +5,7 @@ public class ProjectileController : MonoBehaviour {
 
     public float ProjectileSpeed;
     public GameObject Projectile;
+    public Transform projectileSpawn;
 	// Use this for initialization
 	void Start () {
 	
@@ -13,11 +14,15 @@ public class ProjectileController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
+        Vector2 force = new Vector2(ProjectileSpeed, 0);
+        Vector3 newForce = transform.localRotation * force;
+
         if (Input.GetKey(KeyCode.E))
         {
-            Vector3 vSpawnPos = new Vector3(gameObject.transform.position.x + 0.1f, gameObject.transform.position.y, gameObject.transform.position.z);
-            GameObject Clone = Instantiate(Projectile, vSpawnPos, gameObject.transform.rotation) as GameObject;
-            Clone.GetComponent<Rigidbody2D>().AddForce(new Vector2(ProjectileSpeed, 0));
+            //Vector3 vSpawnPos =  new Vector3(gameObject.transform.localPosition.x + 0.1f, gameObject.transform.localPosition.y, gameObject.transform.localPosition.z);
+
+            GameObject Clone = Instantiate(Projectile, projectileSpawn.position, gameObject.transform.rotation) as GameObject;
+            Clone.GetComponent<Rigidbody2D>().AddForce(newForce);
         }
 
 	
